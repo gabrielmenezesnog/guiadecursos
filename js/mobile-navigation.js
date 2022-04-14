f412cf6474287e649f15d9fb53835d615172fec9
gabrielmenezesnog <gabriel.gmnogueira@gmail.com>
2022-04-14 20:15:07 -0300

    Added accessibility defaults in the menu

--- js/mobile-navigation.js
+++ js/mobile-navigation.js
@@ -1,9 +1,18 @@
 const buttonMobile = document.getElementById("button-mobile");
 
-function toggleMenu() {
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
   const nav = document.getElementById("nav");
   nav.classList.toggle("active");
-  console.log(nav);
+
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir menu");
+  }
 }
 
 buttonMobile.addEventListener("click", toggleMenu);
+buttonMobile.addEventListener("touchstart", toggleMenu);
